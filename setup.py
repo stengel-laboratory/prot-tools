eb0c332a9b3a96db61a72232d12683013b613d67
Kai-Michael Kammer <kai-michael.kammer@uni-konstanz.de>
2023-09-06 15:54:12 +0200

    Update Readme and setup

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ import setuptools
 
 setuptools.setup(name='prot_tools',
       description='Miscellaneous selection of scripts extracting information from pdb files',
-      author='Kai Kammer',
+      author='Kai-Michael Kammer',
       author_email='kai-michael.kammer@uni-konstanz.de',
-      url='https://git.uni-konstanz.de/kai-michael-kammer/prot_tools',
+      url='https://github.com/stengel-laboratory/prot-tools',
       version=versioneer.get_version(),
       cmdclass=versioneer.get_cmdclass(),
       packages=setuptools.find_packages(),
@@ -14,7 +14,12 @@ setuptools.setup(name='prot_tools',
           'numpy',
           'pandas',
           'biopython',
+          'scipy',
+          'matplotlib',
+          'python-libsbml'
+          'jinja2',
+          'plotly',
       ],
       license='MIT',
-      python_requires='>=3.6'
+      python_requires='>=3.10'
       )
