3efb957ceaa3f7b5c3d0804830e7b008ea8f5d4d
Kai Kammer <29574032+kaisengit@users.noreply.github.com>
2026-08-03 20:18:38 +0200

    fix(packaging): correct prot-tools dependency metadata
    Repair the dependency declaration so installation resolves the declared preprocessing requirements correctly.

--- setup.py
+++ setup.py
@@ -2,25 +2,26 @@ import versioneer
 import setuptools
 
 
-setuptools.setup(name='prot_tools',
-      description='Miscellaneous selection of scripts extracting information from pdb files',
-      author='Kai-Michael Kammer',
-      author_email='kai-michael.kammer@uni-konstanz.de',
-      url='https://github.com/stengel-laboratory/prot-tools',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      packages=setuptools.find_packages(),
-      install_requires=[
-          'numpy',
-          'pandas',
-          'biopython',
-          'scipy',
-          'matplotlib',
-          'python-libsbml'
-          'jinja2',
-          'plotly',
-          'pkai'
-      ],
-      license='MIT',
-      python_requires='>=3.10'
-      )
+setuptools.setup(
+    name="prot_tools",
+    description="Miscellaneous selection of scripts extracting information from pdb files",
+    author="Kai-Michael Kammer",
+    author_email="kai-michael.kammer@uni-konstanz.de",
+    url="https://github.com/stengel-laboratory/prot-tools",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "biopython",
+        "scipy",
+        "matplotlib",
+        "python-libsbml",
+        "jinja2",
+        "plotly",
+        "pkai",
+    ],
+    license="MIT",
+    python_requires=">=3.10",
+)
